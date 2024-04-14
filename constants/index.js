56cc781e530e85cdf6b27b53e36316393f4b4278
luisRiofrio <lriofrio915@gmail.com>
2024-04-14 08:23:21 -0400

    actualizacion 1 de la plantilla

---
+++ constants/index.js
@@ -0,0 +1,112 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Índice Nasdaq',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Materias Primas',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Acciones',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Criptoactivos',
+  },
+];
+
+export const startingFeatures = [
+  'Matricúlate hoy, es un solo pago sin recargos',
+  'Instala todos los programas que se explican en los vídeos',
+  'Disfruta de las clases y nos vemos en vivo cada domingo',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Consistencia',
+    subtitle:
+        'Convertirse en Trader no sucede de la noche a la mañana, por esta razón nos reunimos de forma online todos los domingos.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Transparencia',
+    subtitle:
+        'Analizamos operaciones reales tomadas en la semana, hacemos bascktesting e interactuamos con todos los miembros del Club.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'NinjaTrader',
+    subtitle:
+        'Ninja Trader es una plataforma de trading galardonada especializada en operaciones con Futuros y Acciones.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'MetaTrader',
+    subtitle:
+        'Es una plataforma multimercado que permite realizar operaciones con programas de trading algorítmico.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'TradingView',
+    subtitle:
+        'Plataforma de trading usada para el análisis técnico de diferentes activos en múltiples mercados.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Interactive Broker',
+    subtitle:
+        'Institución financiera regulada por la US Securities and Exchange Commission. Broker de confianza para invertir en acciones, opciones, futuros y bonos.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Darwinex',
+    subtitle:
+        'Es una gestora y broker multiactivo fundada en 2012. Empresa privada que da acceso a capital semilla a traders con visión de largo plazo.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Binance',
+    subtitle:
+        'Binance Holdings es una compañía global ​que opera la más grande empresa de intercambio en términos de volumen diario de trading de criptomonedas. ',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Swiset',
+    subtitle:
+        'Es una poderosa herramienta de trading para medir y gestionar la operativa diaria del trader de forma automatizada.',
+  },
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Replicadores BrunoMeza',
+    subtitle:
+        'Estos avanzados replicadores permiten realizar operaciones de trading desde una única cuenta hacia varias cuentas de otros brokers.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+    link: 'https://twitter.com/VigneshGupta9',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/in/vigneshgupta/',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://www.instagram.com/_viggi.g_/',
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText
+        title="| Liberty Trading"
+        textStyles="text-center"
+      />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold"> Liberty </span> es una comunidad de traders que se reune cada domingo por Discord para hacer cosas de traders, realizamos el backtesting semanal, revisamos gráficas, solventamos dudas de los miembros del club, analizamos los <span className="font-extrabold">proyectos cripto</span> de nuestro portafolio. <span className="font-extrabold">En el Club de Trading</span> siempre tienes disponible las clases actualizadas para aprender la estrategia de day trading y la de <span className="font-extrabold">criptoactivos.</span>
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow-down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
